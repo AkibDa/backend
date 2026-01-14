c2deca422d3dbb9966ced557baff61f9dfe8f706
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2026-01-14 12:09:52 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -40,8 +40,13 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5000",
-        "http://127.0.0.1:5000",
+<<<<<<< HEAD
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",     
+        "http://127.0.0.1:5173",
+=======
+>>>>>>> 996c533f5892c8edcdcaa3b3e8b80b85a4050bbc
     ],
     allow_credentials=True,
     allow_methods=["*"],
